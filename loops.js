3a461b2b1925373b6f8ca1b5f726f16661c02829
William Chabla <w_chabla@hotmail.com>
2018-03-09 22:19:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 function forLoop (arrString) {
   for (let i = 0; i < 25; i++) {
     if (i === 0) {
-      arrString.push[i] = "I am 1 strange loop.";
+      arrString.push("I am 1 strange loop.");
     }
     else {
-      arrString.push[i] = `"I am ${i} strange loops."`;
+      arrString.push(`"I am ${i} strange loops."`);
     }
   }
 }
